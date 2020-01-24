0cce29d73d7b5190bc3d45274e0e30bcee215d9f
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-01-24 22:13:23 +0530

    updating

--- test/throttle.js
+++ test/throttle.js
@@ -1,3 +1,33 @@
 var pot = require('pot');
 
-pot.throttlit('realestates', 'realestates');
+var domain = 'realestates';
+var model = 'realestates';
+
+pot.throttlit('realestates', 'realestates', {
+  apis: {
+    bumpup: {
+      second: 0,
+      day: 1,
+      month: 2
+    }
+  },
+  ips: {
+    bumpup: {
+      second: 0,
+      minute: 1,
+      hour: 2,
+      day: 3
+    }
+  }
+}, {
+  bumpup: {
+    POST: function (i) {
+      return {
+        url: pot.resolve(domain, '/apis/v/' + model + '/dummy'),
+        headers: {
+          'X-Action': 'bumpup'
+        }
+      }
+    }
+  }
+});
